c9cc911a30a3a6d3164615902cb8c505a4ab578d
范征远 <1954418428@qq.com>
2022-10-23 19:16:55 +0800

    Feat：底部栏组件静态封装

---
+++ src/components/FloorBar/FloorBar.jsx
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+import './index.less'
+
+import utils from '../../util/utils'
+
+function FloorBar(props){
+
+
+  return (
+    <React.Fragment>
+      <div className="floor-bar flex justify-content-center">
+        <div class="floor-content">
+          <h3>致力打造专注计算机知识教育的免费平台</h3>
+          <h3>怀化学院</h3>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default FloorBar
